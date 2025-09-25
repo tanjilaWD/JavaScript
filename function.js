38f337c5052d2d3d71a3cd3daa792fcf4c12c8bf
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-09-25 19:13:15 +0600

    new

---
+++ function.js
@@ -0,0 +1,11 @@
+function sumOfNumbers(){
+
+}
+const result = sumOfNumbers();
+console.log(result);
+//
+function sumOfNumber(number){
+    console.log(number);
+}
+const Result = sumOfNumber([12,45,6,8,14]);
+console.log(Result);
