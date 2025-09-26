b1fd06ab42081972eb2c0ab579495dea953d6e7e
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-09-26 15:19:34 +0600

    new

--- solving.js
+++ solving.js
@@ -4,3 +4,18 @@ function inchToFeet(inch){
 }
 const tanjilaToFeet = inchToFeet(75);
 console.log(tanjilaToFeet);
+//
+function inchToFeet2(inch){
+    const feetFraction = inch / 12;
+    const feetNumber = parseInt(feetFraction);
+    const inchRemaining = inch % 12;
+    const result = feetNumber + 'ft' + inchRemaining + 'inch.';
+    return result;
+}
+const tanjilaHeight = inchToFeet2(75);
+console.log(tanjilaHeight);
+//
+function mileToKilometer(mile){
+    const kilo = mile * 1.60934;
+    return kilo;
+}
