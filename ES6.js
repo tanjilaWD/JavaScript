a7eb65a51d99d5c4bf47b396facbe2a577f75845
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-09-26 18:21:59 +0600

    new

--- ES6.js
+++ ES6.js
@@ -1,3 +1,24 @@
 const money = 25;
 const rich = money + 25;
 console.log(rich);
+//
+let count = 0;
+count = count + 10;
+console.log(count);
+//
+const numbers = [23,4,23,12,56];
+numbers[1] = 55;
+console.log(numbers);
+//
+const number = [23,4,23,12,56];
+number[1] = 55;
+number.push(8,9,65);
+console.log(number);
+//
+const student ={
+    name: 'Tanjila',
+    class: 12,
+}
+student.name = 'Bithi';
+console.log(student);
+//
