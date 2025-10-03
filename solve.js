95bcb264bb4dfbf7a8c5925a84f2ad581499915c
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-03 19:49:11 +0600

    new code added

--- solve.js
+++ solve.js
@@ -57,3 +57,60 @@ function OddAverage(numbers){
 const numbers11 = [2,4,6,8,10];
 const avg11 = OddAverage(numbers11);
 console.log('Average of the odd numbers is:', avg11);
+//
+function oddAverage(arr){
+    const odds = arr.filter(num => num % 2 !== 0);
+    if(odds.length === 0){
+        return "No odd numbers found";
+    }
+    const sum = odds.reduce((acc,val) => acc + val, 0);
+    return(sum/odds.length).toFixed(2);
+}
+//
+function multiplyoddByTwo(arr){
+    const odds = arr.filter(num => num % 2 !== 0);
+    return odds.map(num=> num * 2);
+}
+//
+function checkodd(arr){
+    const odds = arr.filter(num => num % 2 !== 0);
+    if(odds.length === 0){
+        return 'No odd numbers found';
+    }else{
+        return 'Odd numbers found';
+    }
+}
+//
+const students = ['abul', 'babul','cabul','abul','babul','dabul'];
+// const numbers = [1,5,61,5,87,7,5,81,61];
+function noDuplicate(array){
+    const unique =  [ ];
+    for(const item of array){
+        if(unique.includes(item)=== false){
+            unique.push(item);
+        }
+    }
+    return unique;
+}
+const uniqueArray = noDuplicate(students);
+console.log(uniqueArray);
+//
+const numBers =[1,5,61,5,87,7,5,81,61];
+function NoDuplicate(Array){
+    const unique= [ ];
+    for(const item of Array){
+        if(unique.includes(item) === false){
+            unique.push(item);
+        }
+    }
+    return unique;
+}const uniqueArray1 = NoDuplicate(numBers);
+console.log(uniqueArray1);
+//
+let a = 5;
+let b = 7;
+console.log(a,b);
+const temp = a;
+a = b ;
+b = temp;
+console.log(a,b);
