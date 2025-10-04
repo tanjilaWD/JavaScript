c8857f3098d5a578674657941c1d201752111d70
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-04 17:55:37 +0600

    new code added

---
+++ string.js
@@ -0,0 +1,19 @@
+const first = 'jaan';
+const last = 'pakhi';
+const name = first + ' ' + last + ' ' + 'Tanjila';
+console.log(name);
+//
+const a = 10;
+const b = 20;
+const result = ' The sun of ' + a + ' and ' + b + ' is ' + (a+b);
+console.log(result);
+//
+const Name = 'Tanjila';
+const Age = 30;
+console.log('Name: ' + Name + ' , Age: ' + Age );
+console.log(`Name: ${Name} , Age: ${Age}`);
+//
+const i = 10;
+const j = 20;
+const math = `The sum of ${a} and ${b} is ${a+b}`;
+console.log(math);
