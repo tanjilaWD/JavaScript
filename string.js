4df4f55314b6d278e4511099afaff391bbde3f84
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-04 18:45:51 +0600

    new

--- string.js
+++ string.js
@@ -17,3 +17,40 @@ const i = 10;
 const j = 20;
 const math = `The sum of ${a} and ${b} is ${a+b}`;
 console.log(math);
+//
+const getName = function(){
+    return 'Tanjila';
+}
+const message = `Na bolle Sadia, ase amr ${getName()}!`;
+console.log(message);
+//
+const isAge = 20;
+const status = `You are ${isAge >= 18 ? 'Adult' : 'Tanjila'}`;
+console.log(status);
+//
+const user = {name: 'Bithi', age: 21};
+const info = `Name: ${user.name}, Age: ${user.age}`;
+console.log(info);
+//
+const fruits = ['Apple','Banana','Mango'];
+const second = 'My second favorite fruit is: ${fruits[1]}.';
+const list = `My favorite fruits are: ${fruits.join(',')}.`;
+console.log(list);
+//
+const title = 'Welcome';
+const body = 'This is  a dynamic template.';
+const html = `<div>
+<h1>${title}</h1>
+<p>${body}</p>
+</div>`;
+console.log(html);
+//
+const radius = 5;
+const area = `Circle area is ${Math.PI * radius ** 2}.`;
+console.log(area);
+//
+const greet = function(name){
+    return `Hello, ${name}`;
+};
+console.log(greet('Bithi'));
+//
