6bbf11af8e32aa74146ab4077f3f8e8045f277fa
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-06 18:58:03 +0600

    new file added

---
+++ advanced.js
@@ -0,0 +1,20 @@
+const actor ={
+    name: 'Tanjila',
+    age: 20,
+    phone:'01934129995',
+    money: 123334
+}
+// const name = actor.name;
+// const age = actor.age;
+// const phone = actor.phone;
+// const money = actor.money;
+// console.log(name,age,phone,money);
+//
+const {name,age,money,phone} = actor;
+console.log(name);
+console.log(age);
+console.log(money);
+console.log(phone);
+//
+
+
