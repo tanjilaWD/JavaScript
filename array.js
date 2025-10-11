9b22051a3bb6d6971cfa1f44ef6bed562a30124a
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-11 21:08:18 +0600

    new

--- array.js
+++ array.js
@@ -30,3 +30,6 @@ const Numbers = [1,5,6,4,15];
 const result = Numbers.forEach(n => console.log(n));
 console.log(result);
 //
+const players = [75,65,67,71,55,59];
+const selected = players.filter(p => p>70);
+console.log(selected);
