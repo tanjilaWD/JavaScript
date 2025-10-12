017791eff82066f5f579c83c7c65eff72b251f18
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-12 20:51:46 +0600

    new code

---
+++ every.js
@@ -0,0 +1,13 @@
+// const score = [30,45,55,80,90];
+// const hasPassed = score.some(score => score >= 50);
+// console.log(hasPassed);
+
+//
+const score = [60,70,55,80,90];
+const allPassed = score.every(score => score >= 50);
+console.log(allPassed);
+
+//
+const scores = [60,45,55,80,90];
+const allpassed = scores.every(scores => scores >= 50);
+console.log(allpassed);
