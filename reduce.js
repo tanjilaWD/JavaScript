3a1b9ce110bc10efde62d28a01613660d5b1dc3d
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-13 10:49:25 +0600

    reduce code

---
+++ reduce.js
@@ -0,0 +1,10 @@
+const numbers = [4,5,7,1,2,66];
+let sum = 0;
+for(const num of numbers){
+    sum = sum + num;
+}
+console.log(sum);
+//
+// numbers.reduce((accumulator,currentValue)=> doSomeWork, initialValue)
+
+//
