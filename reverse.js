9ea53ad9e9e2a055c14155dc7fae4c451e8e2cde
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-13 20:30:36 +0600

    reverse code add

---
+++ reverse.js
@@ -0,0 +1,24 @@
+const names = ['Alice', 'Bob','Charlie','David'];
+names.reverse();
+console.log(names);
+//
+const numbers = [10,20,30,40,50];
+numbers.reverse();
+console.log(numbers);
+//
+const name = ['Zara','Tariq','Amir','Lina'];
+name.reverse();
+console.log(name);
+//
+const num = [1,4,9,7];
+num.reverse();
+console.log(num);
+//
+const users = [
+    {name:'Ali', age:30},
+    {name:'Sara', age:25},
+    {name:'Tariq', age:35}
+];
+users.reverse();
+console.log(users);
+
