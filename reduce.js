81329f44613eef4333ea00cdc2790c927353baa7
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-13 11:51:40 +0600

    new

--- reduce.js
+++ reduce.js
@@ -33,3 +33,35 @@ console.log(affordable);
 
 const totalll = products.reduce((accumolator, current) => accumolator + current.price, 0);
 console.log(totalll);
+//
+const nums = [5,10,15,20,25];
+const totalValue = nums.reduce((accumolator,  current)=> accumolator + current, 0);
+console.log(totalValue);
+//
+const owner = [
+    {name: 'shampoo', price: 100},
+    {name: 'soap', price: 50},
+    {name: 'toothpaste', price: 75}
+];
+const totalPrice = owner.reduce((accumolator, current)=> accumolator + current.price, 0);
+console.log(totalPrice);
+//
+const product = [
+    {name:'Pen', price:5},
+    {name: 'Book', price: 50},
+    {name: 'Bag', price:100}
+];
+const productPrice = product.reduce((accumolator, current)=> accumolator + current.price,0);
+console.log(productPrice);
+//
+const numbers1 =[1,2,3,4,5];
+const add = numbers1.reduce((accumolator,current)=> accumolator * current, 1);
+console.log(add);
+//
+const numbers2 =[10,20,30,40,50];
+const maxValue = numbers2.reduce((max, current)=> current > max ? current : max);
+console.log(maxValue);
+//
+const numbers3 = [100,200,300];
+const summ = numbers3.reduce((accumolator,current)=> accumolator + current, 50);
+console.log('Sumation =', summ);
