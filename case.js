13225062e7f26aef38eb246532ddf4afd1056a19
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-14 10:36:34 +0600

    case code add

---
+++ case.js
@@ -0,0 +1,40 @@
+// const subject = 'Chemistry';
+// const book = 'chemistry';
+// if(subject === book){
+//     console.log('i am reading book');
+// }else{
+//     console.log('hudai prista ultai');
+// }
+//
+const name  = 'noMAD d MAD';
+const lower = name.toLowerCase();
+console.log(lower);
+//
+const nam = 'tanjila';
+const upper = nam.toUpperCase();
+console.log(upper);
+//
+const subject = 'Chemistry';
+const book = 'chemistry';
+if(subject.toLowerCase() === book.toLowerCase()){
+    console.log('i am reading book');
+}else{
+    console.log('hudai');
+}
+//
+const email = 'User@Example.Com';
+const lowerEmail = email.toLowerCase();
+console.log(lowerEmail);
+//
+const greeting = 'hELlo WoRLd';
+const lo = greeting.toLowerCase();
+const up = greeting.toUpperCase();
+console.log(lo);
+console.log(up);
+//
+const lang = 'JavaScript';
+console.log(lang.toLowerCase().includes('script'));
+//
+const text = 'NodeJs';
+const firstChar = text.charAt(0);
+console.log(firstChar === firstChar.toUpperCase());
