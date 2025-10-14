5a597fc364b1f859fc5f6b60ba3cd4042d7fb02f
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-14 09:56:12 +0600

    mutable & Immutable code add

---
+++ string0.js
@@ -0,0 +1,34 @@
+const capital = 'Dha ka';
+console.log(capital.length);
+//
+const capital0 = 'Dhaka';
+console.log(capital[0]);
+//
+const name = 'Abid';
+console.log(name.includes('d'));
+//
+const language = 'javaScript';
+console.log(language.indexOf('rip'));
+console.log(language.indexOf('py'));
+//
+const numbers = [54,98,78,21,65];
+numbers[1]= 11;
+console.log(numbers);
+//
+const capitall = 'Dhaka';
+capitall[0] = 'F';
+console.log(capitall);
+//
+const city = 'Chattogram';
+console.log(city.indexOf('g'));
+//
+const division = 'Sylhet';
+console.log(division.includes('y'));
+//
+const nam = 'Rifat';
+console.log(nam['4']);
+//
+const text = 'Immutable';
+console.log(text.length);
+text[1]= 'k' ;
+console.log(text);
