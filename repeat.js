c97b44b2f1b2dd6c0e7b158a3f743262157c12a6
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-15 08:47:13 +0600

    repeat code add

---
+++ repeat.js
@@ -0,0 +1,29 @@
+const str = ' Hello !';
+const repeatedStr = str.repeat(3);
+console.log(repeatedStr);
+//
+const num = 123;
+const stR = num.toString();
+console.log(stR);
+//
+const text = 'I love programing: programing is fun';
+const result = text.replace('programing','coding');
+console.log(result);
+//
+const text1 = 'I love programing: programing is fun';
+const result1 = text1.replaceAll('programing','coding');
+console.log(result1);
+//
+const i = ' Hi ';
+const repeatedI = i.repeat(10);
+console.log(repeatedI);
+//
+const y = 'I am learning JavaScript.JavaScript is amazing';
+const repY = y.replace('JavaScript','JS');
+console.log(repY);
+//
+const apple = 'I love apples and apples are tasty';
+const repApple = apple.replaceAll('apples','mangoes');
+console.log(repApple);
+
+
