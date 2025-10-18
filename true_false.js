8dcdbb2cf9056c0aa93685ae84a0776ea3713e1f
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-18 20:43:21 +0600

    new

---
+++ true_false.js
@@ -0,0 +1,19 @@
+const x = -4;
+if(x){
+    console.log('value of x is truthy');
+}else{
+    console.log('value of x is falsy');
+}
+//
+const i = 'a';
+if(i){
+    console.log('value of x is truthy');
+}else{
+    console.log('value of x is falsy');
+}
+//
+const y = ' ';
+if(!y){
+    console.log('value of y is falsy');
+}
+//
