fb9cc03346719517bac3386a79259aab9dc92c69
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-21 19:06:58 +0600

    type constructor

---
+++ type_constructor.js
@@ -0,0 +1,42 @@
+const strNum = '42';
+const num = Number(strNum);
+console.log(num);
+//
+const str = '343';
+const nums = +str;
+console.log(typeof num);
+//
+const Num = 123;
+const Str = String(Num);
+console.log(Str);
+//
+const n = 232;
+const s = n + " ";
+console.log(typeof s);
+//
+const isTruethy = Boolean(1);
+console.log(isTruethy);
+//
+const add = new Function('a','b','return a + b');
+console.log(add(5,10));
+//
+const person = new Object();
+person.name = 'John';
+person.age = 30;
+console.log(person);
+//
+const start = '786';
+const startNum = Number(start);
+console.log(startNum);
+//
+const willAttend = Boolean(0);
+console.log(willAttend);
+//
+const i = new Object();
+i.city = 'Khulna';
+i.country = 'Bangladesh';
+console.log(i);
+//
+const total = 1970;
+const totalstr = String(total);
+console.log(totalstr);
