f7f45fcabe9ae20ea6075237df574d4f74ed6fcd
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-22 01:14:26 +0600

    math code

---
+++ math1.js
@@ -0,0 +1,43 @@
+const min = Math.min(45,21,65,99,126,5);
+console.log(min);
+//
+const max = Math.max(45,21,65,99,126,5);
+console.log(max);
+//
+console.log("Value of PI is:", Math.PI);
+//
+console.log(Math.abs(-4));
+console.log(Math.abs(4));
+//
+console.log(Math.round(4.7));
+console.log(Math.round(4.3));
+//
+console.log(Math.floor(4.7));
+console.log(Math.floor(4.2));
+//
+console.log(Math.ceil(4.7));
+console.log(Math.ceil(4.2));
+//
+console.log(Math.random());
+//
+const num = Math.min(45,11,89,23,56,-12,-56);
+console.log(num);
+//
+const bigNum = Math.max(21,35,67);
+console.log(bigNum);
+//
+console.log(Math.round(7.6));
+console.log(Math.round(7.2));
+//
+console.log(Math.floor(9.8));
+console.log(Math.floor(5.3));
+//
+console.log(Math.ceil(3.1));
+console.log(Math.ceil(6.9));
+//
+console.log(Math.abs(-34));
+//
+const nums = 3.5;
+console.log(Math.round(nums));
+console.log(Math.floor(nums));
+console.log(Math.ceil(nums));
