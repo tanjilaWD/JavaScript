fbb23d496fbb63812cff14def5167c7b5440b4fb
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-10-27 01:32:04 +0600

    json code

--- json.js
+++ json.js
@@ -48,3 +48,44 @@ const cart = {
 };
 console.log(JSON.stringify(cart));
 //
+const weather = {
+    city: 'Dhaka',
+    temperature: 29,
+    forecst:[
+        {day: "Monday", temp:30},
+        {day: "Monday", temp:31},
+        {day: "Monday", temp:28},
+        {day: "Monday", temp:29},
+        {day: "Monday", temp:27},
+        {day: "Monday", temp:30},
+        {day: "Monday", temp:32},
+    ]
+};
+console.log(JSON.stringify(weather));
+//
+const movie ={
+    title: "Inception",
+    releaseYear:['Lenovo Dicaprio','Tom Hardy','Ellen Page'],
+};
+const movieJSON = JSON.stringify(movie);
+console.log(movieJSON);
+
+const movieObjet = JSON.parse(movieJSON);
+console.log(movieObjet);
+//
+const projectSystem ={
+    projects: [
+        {
+            name: " portfolio website ",
+            description: 'A personal web portfolio',
+            teamMembers: ['Tanjila', 'Nayaeem', "Sara"],
+            deadlines: '2025-12-01',
+            tasks:[
+                {title: 'Design UI', assignee: 'Tanjila', status: 'Done'},
+                {title:'Develop Backend', assignee: 'Nayeem', status: 'In Progress'},
+                {title: 'Testing', assignee: 'Sara', status: 'pending'}
+            ]
+        }
+    ]
+};
+console.log(JSON.stringify(projectSystem));
