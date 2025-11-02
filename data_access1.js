bd5a15887d432d9b3841b16a33a5d6b5cc3b9b5d
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-11-02 19:57:59 +0600

    data access

--- data_access1.js
+++ data_access1.js
@@ -33,3 +33,23 @@ console.log(city);
 const poosttalCode = user?.address?.poosttalCode?? 'Postal code not available';
 console.log(poosttalCode);
 //
+let x = null;
+x ??= 75;
+console.log(x);
+//
+let product = {};
+product.stock ??= 0;
+console.log(product.stock);
+//
+let productDetails = {discount: false};
+productDetails.discount ||= 10;
+console.log(productDetails.discount);
+//
+let vehicle = {};
+console.log(vehicle?.engine ?? 'Engine info missing');
+//
+let restaurant = {};
+console.log(restaurant?.menu ?? 'Menu not available');
+//
+let profile = {social: {}};
+console.log(profile?.social?.twitter ?? 'Twitter handle not available');
