347818118d6cb97414e7561596e6f492a82894e1
Tanjila Khatun <tanjilabithi12@gmail.com>
2025-11-03 10:08:44 +0600

    data access

---
+++ data_access2.js
@@ -0,0 +1,32 @@
+// let a = 5;
+// let b = a++;
+// console.log(b);
+
+//
+
+let a = 5;
+let b = ++a;
+console.log(b);
+//
+
+let mango = 10;
+mango &&= 5;
+console.log(mango);
+
+//
+
+let tomato = 0;
+tomato &&= 5;
+console.log(tomato);
+
+//
+
+let money = 0;
+money ||= 5;
+console.log(money);
+
+//
+
+let price = 10;
+price ||= 5;
+console.log(price);
